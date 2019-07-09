705eedd425468e967ea51ecff9dca3c100faeef3
JW <wodn5210@gmail.com>
2019-07-09 17:32:16 +0900

    행렬변환까지

--- DirectX_practice/DirectX_practice/소스.cpp
+++ DirectX_practice/DirectX_practice/소스.cpp
@@ -1,8 +1,11 @@
-#include <d3d9.h>
+#include <Windows.h>
+#include <mmsystem.h>
+#include <d3dx9.h>
 #pragma warning( disable : 4996 ) 
 #include <strsafe.h>
 #pragma warning( default : 4996 )
-#define OBC 6
+
+#define OBC 3
 
 
 
@@ -15,10 +18,10 @@ LPDIRECT3DVERTEXBUFFER9 g_pVB = NULL;								//vertex 버퍼
 
 struct CUSTOMVERTEX
 {
-	FLOAT x, y, z, rhw;
+	FLOAT x, y, z;
 	DWORD color;
 };
-#define D3DFVF_CUSTOMVERTEX (D3DFVF_XYZRHW | D3DFVF_DIFFUSE)
+#define D3DFVF_CUSTOMVERTEX (D3DFVF_XYZ| D3DFVF_DIFFUSE)
 
 //-----------------------------------------------------------------------------
 // Name: InitD3D()
@@ -44,27 +47,29 @@ HRESULT InitD3D(HWND hWnd)
 	{
 		return E_FAIL;
 	}
+	// Turn off culling, so we see the front and back of the triangle
+	g_pd3dDevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
 
+	// Turn off D3D lighting, since we are providing our own vertex colors
+	g_pd3dDevice->SetRenderState(D3DRS_LIGHTING, FALSE);
 
 	return S_OK;
 }
 
 LRESULT InitVB()
 {
-	//정점 버퍼 선언
+
 	CUSTOMVERTEX vertices[] =
 	{
-		{150.0f, 50.0f, 0.5f, 1.0f, 0xffff0000},
-		{250.0f, 250.0f, 0.5f, 1.0f, 0xff00ff00},
-		{250.0f, 250.0f, 0.5f, 1.0f, 0xff00ffff},
-		{50.0f, 250.0f, 0.5f, 1.0f, 0xff00ffff},
-		{50.0f, 250.0f, 0.5f, 1.0f, 0xff00ffff},
-		{150.0f, 50.0f, 0.5f, 1.0f, 0xff00ffff}
+		{ -1.0f,-1.0f, 0.0f,0xffff0000, },
+		{  1.0f,-1.0f, 0.0f, 0xff0000ff, },
+		{  0.0f, 1.0f, 0.0f, 0xffffffff, },
 	};
 
 	//정점 버퍼 생성
 	//CreateVertexBuffer(사이즈, 버퍼처리방식-SW/HW, FVF플래그, 정점버퍼 메모리 위치, 반환될 버퍼의 인터페이스) -> 이 코드는 GPU메모리에 보관
-	if (FAILED(g_pd3dDevice->CreateVertexBuffer(OBC * sizeof(CUSTOMVERTEX), 0, D3DFVF_CUSTOMVERTEX, D3DPOOL_DEFAULT, &g_pVB, NULL)))
+	if (FAILED(g_pd3dDevice->CreateVertexBuffer(OBC * sizeof(CUSTOMVERTEX), 
+		0, D3DFVF_CUSTOMVERTEX, D3DPOOL_DEFAULT, &g_pVB, NULL)))
 	{
 		return E_FAIL;
 	}
@@ -102,7 +107,29 @@ VOID Cleanup()
 		g_pD3D->Release();
 }
 
+VOID SetupMatrices()
+{
+	//월드행렬
+	D3DXMATRIXA16 matWorld;
 
+	UINT iTime = timeGetTime() % 1000;						// float연산 정밀도 위해서 나머지연산
+	FLOAT fAngle = iTime * (2.0f * D3DX_PI) / 1000.0f;		// 1초마다 한바퀴씩 회전하는 행렬
+	D3DXMatrixRotationY(&matWorld, fAngle);					// Y축을 회전축으로 회전 행렬을 생성
+	g_pd3dDevice->SetTransform(D3DTS_WORLD, &matWorld);		// 생성한 회전행렬을 월드 행렬로 디바이스에 설정
+
+	D3DXVECTOR3 vEyePt(0.0f, 3.0f, -5.0f);						// 눈위치
+	D3DXVECTOR3 vLookatPt(0.0f, 0.0f, 0.0f);					// 눈이 바라보는 위치
+	D3DXVECTOR3 vUpVec(0.0f, 1.0f, 0.0f);						// 천장 방향을 나타내는 벡터
+
+	D3DXMATRIXA16 matView;			
+	D3DXMatrixLookAtLH(&matView, &vEyePt, &vLookatPt, &vUpVec);		// 1, 2, 3의 값으로 뷰 행렬 생성
+	g_pd3dDevice->SetTransform(D3DTS_VIEW, &matView);				// 생성한 뷰 행렬을 디바이스에 설정
+
+	//프로젝션 행렬 정의: 시야각(Field Of View), 종횡비(aspect ratio), 클리핑 평면 값
+	D3DXMATRIXA16 matProj;
+	D3DXMatrixPerspectiveFovLH(&matProj, D3DX_PI / 4, 1.0f, 1.0f, 100.0f);		//(입력할 프로젝션 행렬, 시야각(45도), 종횡비, 근접 ~ 원거리 클리핑 평면
+	g_pd3dDevice->SetTransform(D3DTS_PROJECTION, &matProj);					//프로젝션 행렬을 디바이스에 설정
+}
 
 
 //-----------------------------------------------------------------------------
@@ -117,16 +144,20 @@ VOID Render()
 	// Clear the backbuffer to a blue color
 	g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);			//화면 한 색으로 초기화
 
+	SetupMatrices();																		//월드, 뷰, 프로젝션 행렬 설정
+
 	// Begin the scene
 	if (SUCCEEDED(g_pd3dDevice->BeginScene()))													//폴리곤 그린다고 알리기
 	{
+
+
 		// 삼각형 그리기 시작함
 		//1. 정점정보가 담겨있는 정정버퍼를 출력 스트림으로 할당									정점버퍼 디바이스에 결함
 		g_pd3dDevice->SetStreamSource(0, g_pVB, 0, sizeof(CUSTOMVERTEX));
 		//2. D3D에게 정점 셰이더 정보 지정 - 대부분 FVF만 지정										정점 포맷을 디바이스에 지정
 		g_pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX);
 		//3. 기하정보 출력하는 DrawPrimitive() 함수 호출											가장 성능 좋은것은 DrawIndexedPrimitive()								정점버퍼의 폴리곤 그림
-		g_pd3dDevice->DrawPrimitive(D3DPT_LINESTRIP, 0, 5);
+		g_pd3dDevice->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
 
 		// End the scene
 		g_pd3dDevice->EndScene();																	//폴리곤 다 그림
@@ -184,7 +215,7 @@ INT WINAPI wWinMain(HINSTANCE hInst, HINSTANCE, LPWSTR, INT)
 
 	// Create the application's window
 	HWND hWnd = CreateWindow(L"D3D Tutorial", L"D3D Tutorial 01: CreateDevice",
-		WS_OVERLAPPEDWINDOW, 100, 100, 300, 300,
+		WS_OVERLAPPEDWINDOW, 100, 100, 256, 256,
 		NULL, NULL, wc.hInstance, NULL);
 
 	// Initialize Direct3D
@@ -200,11 +231,6 @@ INT WINAPI wWinMain(HINSTANCE hInst, HINSTANCE, LPWSTR, INT)
 			// Enter the message loop
 			MSG msg;							//메세지 루프는 OS가 메세지를 보냈을 경우 사용자 윈도우에 보내는것
 			ZeroMemory(&msg, sizeof(msg));
-			/*while (GetMessage(&msg, NULL, 0, 0))
-			{
-				TranslateMessage(&msg);
-				DispatchMessage(&msg);
-			}*/
 			while (msg.message != WM_QUIT)			//종료 메세지 아니면 계속돈다
 			{
 				if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
